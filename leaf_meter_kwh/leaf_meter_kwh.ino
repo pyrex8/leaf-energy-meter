7c9c67175e68624b572571ede47836a9b1c194f8
pyrex8 <ev64bug@yahoo.com>
2023-08-06 20:52:07 -0700

    add screen sleep after 1 minute

--- leaf_meter_kwh/leaf_meter_kwh.ino
+++ leaf_meter_kwh/leaf_meter_kwh.ino
@@ -16,7 +16,10 @@
 MCP_CAN CAN0(10);
 #define COUNTS_PER_DECI_AH 7200
 
-#define frac(x) (int(100*(x - int(x))))
+#define DELAY_1MS 1
+#define SLEEP_TIMEOUT_MINUTES 1
+#define SLEEP_COUNTER_MS 1000
+#define SLEEP_COUNTER_SEC (SLEEP_TIMEOUT_MINUTES * 60)
 
 long unsigned int rxId;
 unsigned char len = 0;
@@ -28,12 +31,22 @@ int16_t ah = 0;
 int16_t ah_frac = 0;
 int32_t ah_count = 0;
 int16_t ah_deci = 0;
+uint16_t screen_sleep_counter_ms = 0;
+uint16_t screen_sleep_counter_sec = 0;
+bool screen_asleep = true;
 
 char buf_int[4];
 char buf_frac[4];
 
 void oled_update()
 {
+  if (screen_asleep)
+  {
+    screen_asleep = false;
+    ssd1306_printFixed2x(88, 16, "Ah", STYLE_BOLD);
+    ssd1306_printFixed2x(41, 16, ".", STYLE_BOLD);
+  }
+
   ah_deci = (int16_t)(ah_count / COUNTS_PER_DECI_AH);
   if (ah_count < 0)
   {
@@ -58,8 +71,6 @@ void setup()
   ssd1306_128x64_i2c_init();
   ssd1306_setFixedFont(ssd1306xled_font8x16);
   ssd1306_clearScreen();    
-  ssd1306_printFixed2x(88, 16, "Ah", STYLE_BOLD);
-  ssd1306_printFixed2x(41, 16, ".", STYLE_BOLD);
   CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_8MHZ);
   CAN0.init_Mask(0, 0, 0x07ff0000);
   CAN0.init_Filt(0, 0, 0x01db0000);
@@ -92,6 +103,24 @@ void loop()
         ah_count += (int32_t)amp;
       }
       oled_update();
+      screen_sleep_counter_ms = 0;
+      screen_sleep_counter_sec = 0;
     }
+  }
+  else
+  {
+    screen_sleep_counter_ms++;
+    if (screen_sleep_counter_ms > SLEEP_COUNTER_MS)
+    {
+      screen_sleep_counter_ms = 0;
+      screen_sleep_counter_sec++;
+      if (screen_sleep_counter_sec > SLEEP_COUNTER_SEC)
+      {
+        screen_sleep_counter_sec = 0;
+        ssd1306_clearScreen();
+        screen_asleep = true;
+      }
+    }
+    delay(DELAY_1MS);
   }
 }
