f448b41d9c4bdba68bac4303d7090aa3c2006049
pyrex8 <ev64bug@yahoo.com>
2023-08-09 21:31:44 -0700

    trip Ah added

--- leaf-energy-meter.ino
+++ leaf-energy-meter.ino
@@ -13,6 +13,8 @@ MCP_CAN CAN0(10);
 #define Y0 0
 #define Y1 (Y0 + LINE_SPACING)
 
+#define SLEEP_COUNT_MAX 4000
+
 #define TEST_POINT        3
 #define TEST_POINT_INIT   pinMode(TEST_POINT, OUTPUT)
 #define TEST_POINT_HIGH   digitalWrite(TEST_POINT, HIGH)
@@ -26,9 +28,13 @@ int16_t amp = 0;
 int16_t ah = 0;
 int16_t ah_frac = 0;
 int32_t ah_count = 0;
+int32_t ah_count_trip = 0;
 int16_t ah_deci = 0;
 
-char buffer[10];
+bool screen_asleep = true;
+uint16_t screen_sleep_counter = 0;
+
+char buffer[17];
 char buff_char[2] = "0";
 uint8_t i = 0;
 
@@ -44,7 +50,8 @@ void setup()
   CAN0.setMode(MCP_NORMAL);
 
   pinMode(CAN0_INT, INPUT);
-  sprintf(buffer, " %2d.%02dAh", ah, ah_frac);
+  sprintf(buffer, "-%2d.%02d Ah ", ah, ah_frac);
+  sprintf(&buffer[10], "-%2d.%02d", ah, ah_frac);
 }
 
 void loop()
@@ -70,19 +77,44 @@ void loop()
         ah_count += (int32_t)amp;
       }
 
+      screen_sleep_counter = 0;
+
+      if (screen_asleep && (ah_count >= 0))
+      {
+        screen_asleep = false;
+        ah_count_trip = 0;
+      }
+      
+      ah_count_trip += (int32_t)amp;
+
       ah_deci = (int16_t)(ah_count / COUNTS_PER_DECI_AH); 
       if (ah_count < 0)
       {
         ah_deci = -ah_deci;
         ah = ah_deci / 100;
         ah_frac = ah_deci % 100;
-        sprintf(buffer, "-%2d.%02dAh", ah, ah_frac);
+        sprintf(buffer, "-%2d.%02d Ah", ah, ah_frac);
       }
       else
       {
         ah = ah_deci / 100;
         ah_frac = ah_deci % 100;
-        sprintf(buffer, " %2d.%02dAh", ah, ah_frac);
+        sprintf(buffer, " %2d.%02d Ah ", ah, ah_frac);
+      }
+
+      ah_deci = (int16_t)(ah_count_trip / COUNTS_PER_DECI_AH); 
+      if (ah_count < 0)
+      {
+        ah_deci = -ah_deci;
+        ah = ah_deci / 100;
+        ah_frac = ah_deci % 100;
+        sprintf(&buffer[10], "-%2d.%02d", ah, ah_frac);
+      }
+      else
+      {
+        ah = ah_deci / 100;
+        ah_frac = ah_deci % 100;
+        sprintf(&buffer[10], " %2d.%02d", ah, ah_frac);
       }
     }
   }
@@ -90,12 +122,19 @@ void loop()
   {
     TEST_POINT_HIGH;
     i++;
-    if (i > 7)
+    if (i > 15)
     {
       i = 0;
     }
     buff_char[0] = buffer[i];
     ssd1306_printFixed(i*8, Y0, buff_char, STYLE_BOLD);
     TEST_POINT_LOW;
+
+    screen_sleep_counter++;
+    if (screen_sleep_counter == SLEEP_COUNT_MAX)
+    {
+      screen_asleep = true; 
+      screen_sleep_counter = 0;
+    }
   }
 }
