68d3afb51c30482b135b3cec0e397f1128cc5351
pyrex8 <ev64bug@yahoo.com>
2023-08-10 20:25:07 -0700

    added kWr and trip kWh back in

--- leaf-energy-meter.ino
+++ leaf-energy-meter.ino
@@ -20,23 +20,41 @@ MCP_CAN CAN0(10);
 #define TEST_POINT_HIGH   digitalWrite(TEST_POINT, HIGH)
 #define TEST_POINT_LOW    digitalWrite(TEST_POINT, LOW)
 
+#define KWH_FACTOR 77.5F
+#define frac(x) (int(10*(x - int(x))))
+
 long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
 
 int16_t amp = 0;
 int16_t ah = 0;
+int16_t ah_deci = 0;
 int16_t ah_frac = 0;
 int32_t ah_count = 0;
-int32_t ah_count_trip = 0;
-int16_t ah_deci = 0;
 
-bool screen_asleep = true;
+int16_t ah_trip = 0;
+int16_t ah_deci_trip = 0;
+int16_t ah_frac_trip = 0;
+int32_t ah_count_trip = 0;
+
+uint16_t soc;
+float kwh = 0.0f;
+float kwh_start = 0.0f;
+float kwh_trip = 0.0f;
+float miles = 0.0;
+float mpkwh = 0.0;
+char buf_int[4];
+char buf_frac[4];
+
+bool screen_asleep_ah = true;
+bool screen_asleep_kwh = true;
 uint16_t screen_sleep_counter = 0;
 
-char buffer[17];
+char buffer[100];
 char buff_char[2] = "0";
 uint8_t i = 0;
+uint8_t j = 0;
 
 void setup()
 {
@@ -47,11 +65,9 @@ void setup()
   CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_8MHZ);
   CAN0.init_Mask(0, 0, 0x07ff0000);
   CAN0.init_Filt(0, 0, 0x01db0000);
+  CAN0.init_Filt(1, 0, 0x05bc0000);
   CAN0.setMode(MCP_NORMAL);
-
   pinMode(CAN0_INT, INPUT);
-  sprintf(buffer, "-%2d.%02d Ah ", ah, ah_frac);
-  sprintf(&buffer[10], "-%2d.%02d", ah, ah_frac);
 }
 
 void loop()
@@ -79,9 +95,9 @@ void loop()
 
       screen_sleep_counter = 0;
 
-      if (screen_asleep && (ah_count >= 0))
+      if (screen_asleep_ah && (ah_count >= 0))
       {
-        screen_asleep = false;
+        screen_asleep_ah = false;
         ah_count_trip = 0;
       }
       
@@ -93,48 +109,69 @@ void loop()
         ah_deci = -ah_deci;
         ah = ah_deci / 100;
         ah_frac = ah_deci % 100;
-        sprintf(buffer, "-%2d.%02d Ah", ah, ah_frac);
       }
       else
       {
         ah = ah_deci / 100;
         ah_frac = ah_deci % 100;
-        sprintf(buffer, " %2d.%02d Ah ", ah, ah_frac);
       }
 
-      ah_deci = (int16_t)(ah_count_trip / COUNTS_PER_DECI_AH); 
+      ah_deci_trip = (int16_t)(ah_count_trip / COUNTS_PER_DECI_AH); 
       if (ah_count_trip < 0)
       {
-        ah_deci = -ah_deci;
-        ah = ah_deci / 100;
-        ah_frac = ah_deci % 100;
-        sprintf(&buffer[10], "-%2d.%02d", ah, ah_frac);
+        ah_deci_trip = -ah_deci_trip;
+        ah_trip = ah_deci_trip / 100;
+        ah_frac_trip = ah_deci_trip % 100;
       }
       else
       {
-        ah = ah_deci / 100;
-        ah_frac = ah_deci % 100;
-        sprintf(&buffer[10], " %2d.%02d", ah, ah_frac);
+        ah_trip = ah_deci_trip / 100;
+        ah_frac_trip = ah_deci_trip % 100;
       }
     }
   }
-  else
+  if (rxId == 0x5bc)
   {
-    TEST_POINT_HIGH;
-    i++;
-    if (i > 15)
+    soc = (rxBuf[0] << 2) | (rxBuf[1] >> 6);
+    kwh = (((float)soc) * KWH_FACTOR) / 1000.0F;
+    if (screen_asleep_kwh)
     {
-      i = 0;
+      kwh_start = kwh;
+      screen_asleep_kwh = false;
     }
-    buff_char[0] = buffer[i];
-    ssd1306_printFixed(i*8, Y0, buff_char, STYLE_BOLD);
-    TEST_POINT_LOW;
+    kwh_trip = kwh_start - kwh;
+  }
 
-    screen_sleep_counter++;
-    if (screen_sleep_counter == SLEEP_COUNT_MAX)
+  TEST_POINT_HIGH;
+  if ((i == 0) && (j == 0))
+  {
+    sprintf(&buffer[0],  "-%2d.%02d Ah ", ah_trip, ah_frac_trip);
+    sprintf(&buffer[10], "-%2d.%02d", ah, ah_frac);
+    sprintf(&buffer[16], " %2d.%01d  kWh        ", int(kwh_trip), frac(kwh_trip));
+    sprintf(&buffer[26], " %2d.%01d ", int(kwh), frac(kwh));
+    sprintf(&buffer[32], " %2d.%01d  miles       ", int(miles), frac(miles));
+    sprintf(&buffer[48], " %2d.%01d  m/kWh       ", int(mpkwh), frac(mpkwh));
+  }
+
+  i++;
+  if (i > 15)
+  {
+    i = 0;
+    j++;
+    if (j > 3)
     {
-      screen_asleep = true; 
-      screen_sleep_counter = 0;
+      j = 0;
     }
+  }
+  buff_char[0] = buffer[i + j*16];
+  ssd1306_printFixed(i*8, j*Y1, buff_char, STYLE_BOLD);
+  TEST_POINT_LOW;
+
+  screen_sleep_counter++;
+  if (screen_sleep_counter == SLEEP_COUNT_MAX)
+  {
+    screen_asleep_ah = true;
+    screen_asleep_kwh = true; 
+    screen_sleep_counter = 0;
   }
 }
